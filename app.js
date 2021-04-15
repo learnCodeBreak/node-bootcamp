0c2418b3cfa54f91ef5188dbafb8cc12cc9eff8e
Dhirendra Prajapati <officialprajapati0@gmail.com>
2021-04-15 21:52:51 +0530

    redirecting request

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
     const url = req.url;
+    const method = req.method;
     if (url === '/') {
         res.write('<html>');
         res.write('<head><title>My First Form</title></head>');
         res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
         res.write('</html>');
+        return res.end();
+    }
+    if (url === "/message" && method === "POST") {
+        fs.writeFileSync('message.txt', 'DUMMY DATA');
+        res.statusCode = 302;
+        res.setHeader('Location', '/'); // Redirect to '/'
         return res.end();
     }
     res.setHeader('Content-Type', 'text/html');
