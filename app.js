10204ca9aed767d677c94ed5a253edcc39d703a2
Dhirendra Prajapati <officialprajapati0@gmail.com>
2021-04-16 15:28:37 +0530

    serving files statically

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-const rootDir = require('./util/path');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
@@ -9,6 +8,7 @@ const shopRoutes = require('./routes/shop');
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));    // This will expose public folder
 
 
 app.use(adminRoutes);
@@ -16,7 +16,7 @@ app.use(shopRoutes);
 
 // This is a fallback route when router does not match with any path provided by user
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(3000);
