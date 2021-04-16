80002ebf601536963df5ee7e7e9ca8720b5f6f10
Dhirendra Prajapati <officialprajapati0@gmail.com>
2021-04-16 10:58:17 +0530

    Parsing incoming request

--- app.js
+++ app.js
@@ -1,15 +1,29 @@
-const http = require('http');
+/**
+ * Express does not parse the body from incoming request
+ * So we need body parser to parse body from incoming request and etract data from body
+ * Body parser won't be able to parse file, json data etc. For these we need to handle differently
+ */
+
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+// this is a middleware which encode url of every request 
+// "extended: false" will only allow user defined data in response, it will not allow default express params
+// like Content-Type, statusCode etc
+app.use(bodyParser.urlencoded({extended: false}));
+
 app.use('/add-product', (req, res, next) => {
-    console.log('In another middleware');
-    res.send('<h1>This is Add Product route</h1>');
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
 });
 
+app.use('/product', (req, res) => {
+    console.log(req.body);
+    res.redirect('/');  // redirect is an utility function which allows to change the route
+})
+
 app.use('/', (req, res, next) => {
-    console.log('In another middleware');
     res.send('<h1>This is home route</h1>');
 })
 
