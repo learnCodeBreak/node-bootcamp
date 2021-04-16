e18c1114638fbc3efee455aeb7e96d2cfc3c9276
Dhirendra Prajapati <officialprajapati0@gmail.com>
2021-04-16 11:21:18 +0530

    Limiting the middleware execution to specific http method

--- app.js
+++ app.js
@@ -1,31 +1,25 @@
-/**
- * Express does not parse the body from incoming request
- * So we need body parser to parse body from incoming request and etract data from body
- * Body parser won't be able to parse file, json data etc. For these we need to handle differently
- */
-
 const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
 
-// this is a middleware which encode url of every request 
-// "extended: false" will only allow user defined data in response, it will not allow default express params
-// like Content-Type, statusCode etc
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/add-product', (req, res, next) => {
     res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
 });
 
-app.use('/product', (req, res) => {
+// This will limit only post request on "/product" route
+// So here we are limiting the middleware execution to post method
+// And making sure that '/product' route should handle only post request
+app.post('/product', (req, res) => {
     console.log(req.body);
-    res.redirect('/');  // redirect is an utility function which allows to change the route
+    res.redirect('/');
 })
 
 app.use('/', (req, res, next) => {
     res.send('<h1>This is home route</h1>');
 })
 
-// NOTE: Express app object will handle both like creating server as well as listening to it
+
 app.listen(3000);
