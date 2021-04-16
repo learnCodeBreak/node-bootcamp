a051c566dd80370546eea1eb09aea6173a0aae3e
Dhirendra Prajapati <officialprajapati0@gmail.com>
2021-04-17 01:18:01 +0530

    handlling handlebar templates with dynamic data

--- routes/shop.js
+++ routes/shop.js
@@ -13,7 +13,7 @@ const router = express.Router();
 
 router.get('/', (req, res, next) => {
     const products = adminData.products;
-    res.render('shop', {prods: products, pageTitle: 'Shop', path: '/'});
+    res.render('shop', {prods: products, pageTitle: 'Shop', path: '/', havingProducts: products.length > 0});
 });
 
 module.exports = router;
