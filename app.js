8dfd59840b00f0142e53600d7f75608ace93daf0
Dhirendra Prajapati <officialprajapati0@gmail.com>
2021-04-16 07:33:59 +0530

    server is running on express app

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const http = require('http');
-const routes = require('./routes');
+const express = require('express');
 
+const app = express();  // creates an express application
 
-const server = http.createServer(routes.handler);
+const server = http.createServer(app);
 
 server.listen(3000, () => {
     console.log(`Server is running at http://localhost:3000`);
