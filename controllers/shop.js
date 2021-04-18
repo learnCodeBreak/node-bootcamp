ecd9b1e49e2962e99d9a6b94859ead79bdcc6d3d
Dhirendra Prajapati <officialprajapati0@gmail.com>
2021-04-18 18:27:47 +0530

    Loading product detail data

--- controllers/shop.js
+++ controllers/shop.js
@@ -12,7 +12,9 @@ exports.getProducts = (req, res, next) => {
 
 exports.getProduct = (req, res, next) => {
     const prodId = req.params.productId;
-    console.log('Product: ', prodId);
+    Product.findByID(prodId, (product) => {
+        console.log(product);
+    })
     res.redirect('/');
 }
 
--- models/product.js
+++ models/product.js
@@ -37,4 +37,12 @@ module.exports = class Product {
     static fetchAll(cb) {
         getProductsFromFile(cb);
     }
+
+
+    static findByID(id, cb) {
+        getProductsFromFile((products) => {
+            const product = products.find(prod => prod.id === id);
+            cb(product);
+        })
+    }
 }
