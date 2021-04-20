f10e4c1125fb5e88bd56bb54c4fec1cd1cd71fe8
Dhirendra Prajapati <officialprajapati0@gmail.com>
2021-04-20 21:51:00 +0530

    adding product delete functionality

--- models/product.js
+++ models/product.js
@@ -46,6 +46,17 @@ module.exports = class Product {
         });
     }
 
+    static delete(id) {
+        getProductsFromFile(products => {
+            const updatedProducts = products.filter(prod => prod.id !== id);
+            fs.writeFile(productFilePath, JSON.stringify(updatedProducts), err => {
+                if (!err) {
+                    
+                }
+            })
+        })
+    }
+
     static fetchAll(cb) {
         getProductsFromFile(cb);
     }
--- routes/admin.js
+++ routes/admin.js
@@ -21,4 +21,6 @@ router.post('/add-product', adminController.postAddProduct);
 
 router.post('/edit-product', adminController.postEditProduct);
 
+router.post('/delete-product', adminController.postDeleteProduct);
+
 module.exports = router;
