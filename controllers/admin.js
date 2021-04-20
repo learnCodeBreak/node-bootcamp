6eca6bdec8209a0d1e6af39451ecc8c514e230e0
Dhirendra Prajapati <officialprajapati0@gmail.com>
2021-04-20 18:38:47 +0530

    pre-populating edit product page with data

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,11 +1,10 @@
 const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
-    res.render('admin/add-product', {
+    res.render('admin/edit-product', {
         pageTitle: 'Add Product',
         path: '/admin/add-product',
-        activeAddProduct: true,
-        productCSS: true
+        editing: false
     });
 }
 
@@ -18,6 +17,27 @@ exports.postAddProduct = (req, res, next) => {
     const product = new Product(title, imageUrl, description, price);
     product.save();
     res.redirect('/');
+}
+
+exports.getEditProduct = (req, res, next) => {
+    const editMode = req.query.edit;
+    if (!editMode) {
+        return res.redirect('/');
+    }
+
+    let prodId = req.params.productId;
+    Product.findByID(prodId, (product) => {
+        if (!product) {
+            return res.redirect('/');
+        }
+
+        res.render('admin/edit-product', {
+            pageTitle: 'Add Product',
+            path: '/admin/edit-product',
+            editing: editMode,
+            product: product
+        });
+    })
 }
 
 exports.getProducts = (req, res, next) => {
--- routes/admin.js
+++ routes/admin.js
@@ -13,6 +13,9 @@ router.get('/add-product', adminController.getAddProduct);
 
 router.get('/products', adminController.getProducts);
 
+router.get('/products/:productId', adminController.getEditProduct);
+
+
 // /admin/add-product => POST
 router.post('/add-product', adminController.postAddProduct);
 
