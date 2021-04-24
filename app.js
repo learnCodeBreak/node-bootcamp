b87d1ba6524af0557694bff734c4dabdac9a6690
Dhirendra Prajapati <officialprajapati0@gmail.com>
2021-04-25 02:30:19 +0530

    fetching products

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const errorController = require('./controllers/error');
-const db = require('./util/database');
 
 const app = express();
 
@@ -12,13 +11,6 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-// Connecting with database
-db.execute('SELECT * FROM products')
-    .then(result => {
-        console.log(result);
-    }).catch(err => {
-        console.log(err);
-    })
 
 app.use(bodyParser.urlencoded({
     extended: false
--- controllers/shop.js
+++ controllers/shop.js
@@ -2,13 +2,17 @@ const Product = require('../models/product');
 const Cart = require('../models/cart');
 
 exports.getIndex = (req, res, next) => {
-    Product.fetchAll((products) => {
-        res.render('shop/index', {
-            prods: products,
-            pageTitle: 'Shop',
-            path: '/'
+    Product.fetchAll()
+        .then(([rows, fieldData]) => {
+            res.render('shop/index', {
+                prods: rows,
+                pageTitle: 'Shop',
+                path: '/'
+            })
+        })
+        .catch(err => {
+            console.log(err);
         });
-    });
 }
 
 exports.getProducts = (req, res, next) => {
