ccc4acd87438d04cb1ffd2235afb2395a978b8b9
Dhirendra Prajapati <officialprajapati0@gmail.com>
2021-04-26 20:16:16 +0530

    updating products

--- controllers/admin.js
+++ controllers/admin.js
@@ -32,30 +32,46 @@ exports.getEditProduct = (req, res, next) => {
   }
 
   let prodId = req.params.productId;
-  Product.findByID(prodId, (product) => {
-    if (!product) {
-      return res.redirect('/');
-    }
 
-    res.render('admin/edit-product', {
-      pageTitle: 'Edit Product',
-      path: '/admin/edit-product',
-      editing: editMode,
-      product: product
-    });
-  })
+  Product.findByPk(prodId)
+    .then(product => {
+      if (!product) {
+        return res.redirect('/');
+      }
+
+      res.render('admin/edit-product', {
+        pageTitle: 'Edit Product',
+        path: '/admin/edit-product',
+        editing: editMode,
+        product: product
+      });
+    }).catch(console.log)
 }
 
 exports.postEditProduct = (req, res, next) => {
   const prodId = req.body.productId;
+
   const updatedTitle = req.body.title;
-  const updatedImageUrl = req.body.imageUrl;
   const updatedPrice = req.body.price;
+  const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
 
-  const updatedProduct = new Product(prodId, updatedTitle, updatedImageUrl, updatedDesc, updatedPrice);
-  updatedProduct.save();
-  res.redirect('/admin/products');
+  Product.findByPk(prodId)
+    .then(product => {
+      product.title = updatedTitle;
+      product.price = updatedPrice;
+      product.imageUrl = updatedImageUrl;
+      product.description = updatedDesc;
+
+      // to avoid the next level chaning for save() promise we can return the save() promise
+      // so that we can resolve or reject save() promise in same chain
+      return product.save();
+    })
+    .then(result => {
+      console.log('Product updated successfully');
+      res.redirect('/admin/products');
+    })
+    .catch(console.log)
 }
 
 exports.getProducts = (req, res, next) => {
