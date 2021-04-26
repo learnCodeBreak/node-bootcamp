8abe948bdb03e93835de2ae86828b0aafaa021d6
Dhirendra Prajapati <officialprajapati0@gmail.com>
2021-04-26 22:44:28 +0530

    Created User Model

---
+++ models/user.js
@@ -0,0 +1,21 @@
+const Sequelize = require('sequelize');
+const sequelize = require('../util/database');
+
+const User = sequelize.define('user', {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncremeent: true,
+    allowNull: false,
+    primaryKey: true
+  },
+  name: {
+    type: Sequelize.TEXT,
+    allowNull: false
+  },
+  email: {
+    type: Sequelize.TEXT,
+    allowNull: false
+  }
+});
+
+module.exports = User;
