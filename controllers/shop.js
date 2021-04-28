5e43aa64b71113e83e7707dc4e7118790a7dd067
Dhirendra Prajapati <officialprajapati0@gmail.com>
2021-04-28 20:16:59 +0530

    adding new products to the cart

--- controllers/shop.js
+++ controllers/shop.js
@@ -52,10 +52,36 @@ exports.getProduct = (req, res, next) => {
 
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.findByID(prodId, (product) => {
-    Cart.addProduct(prodId, product.price);
-  })
-  res.redirect('/cart');
+  let fetchedCart;
+
+  req.user
+    .getCart()
+    .then(cart => {
+      fetchedCart = cart;
+      return cart.getProducts({ where: { id: prodId }});
+    })
+    .then(products => {
+      let product;
+      if (products.length > 0) {
+        product = products[0];
+      }
+
+      let newQuantity = 1;
+      if (product) {
+        // Increase the quantity in cart-item
+      }
+
+      return Product.findByPk(prodId)
+        .then(product => {
+          return fetchedCart.addProduct(product, { through: { quantity: newQuantity }});
+        })
+        .then(result => {
+          console.log('Product added to cart successfully');
+          res.redirect('/cart');
+        })
+        .catch(console.log)
+    })
+    .catch(console.log);
 }
 
 exports.getCart = (req, res, next) => {
