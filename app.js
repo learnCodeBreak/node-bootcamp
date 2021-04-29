9b34ee185c8380c849434d37fc959f679002b840
Dhirendra Prajapati <officialprajapati0@gmail.com>
2021-04-30 02:56:13 +0530

    finishing the database connection

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const errorController = require('./controllers/error');
 
-const mongoConnect = require('./util/database');
+const mongoConnect = require('./util/database').mongoConnect;
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-// const adminRoutes = require('./routes/admin');
+const adminRoutes = require('./routes/admin');
 // const shopRoutes = require('./routes/shop');
 
 
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(express.static(path.join(__dirname, 'public'))); // This will expose public folder
 
-// app.use('/admin', adminRoutes);
+app.use('/admin', adminRoutes);
 // app.use(shopRoutes);
 
 // This is a fallback route when router does not match with any path provided by user
@@ -27,7 +27,6 @@ app.use(errorController.get404);
 
 
 // Connecting mongodb and listening to server
-mongoConnect((client) => {
-  console.log(client);
+mongoConnect(() => {
   app.listen(3000);
 })
--- util/database.js
+++ util/database.js
@@ -7,13 +7,27 @@ const MongoCleint = mongodb.MongoClient;
 
 const mongodbURI = "mongodb+srv://node-bootcamp:node-bootcamp@cluster0.5j4dk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
+let _db;
+
 const mongoConnect = (callback) => {
   MongoCleint.connect(mongodbURI)
     .then(client => {
       console.log('MongoDB Connected');
-      callback(client);
+      _db = client.db();
+      callback();
     })
-    .catch(console.log);
+    .catch(err => {
+      console.log(err);
+      throw err;
+    });
 }
 
-module.exports = mongoConnect;
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw 'No database found!';
+}
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
