bf8a952f77704f54c01b00d2901469af50b89984
Dhirendra Prajapati <officialprajapati0@gmail.com>
2021-04-30 02:29:23 +0530

    connecting to mongodb cluster

--- app.js
+++ app.js
@@ -3,21 +3,15 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const errorController = require('./controllers/error');
 
-const sequelize = require('./util/database');
-const Product = require('./models/product');
-const User = require('./models/user');
-const Cart = require('./models/cart');
-const CartItem = require('./models/cart-item');
-const Order = require('./models/order');
-const OrderItem = require('./models/order-item');
+const mongoConnect = require('./util/database');
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+// const adminRoutes = require('./routes/admin');
+// const shopRoutes = require('./routes/shop');
 
 
 app.use(bodyParser.urlencoded({
@@ -25,55 +19,15 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(express.static(path.join(__dirname, 'public'))); // This will expose public folder
 
-app.use((req, res,next) => {
-  User.findByPk(1)
-    .then(user => {
-      req.user = user;
-      next();
-    })
-    .catch(console.log)
-})
-
-app.use('/admin', adminRoutes);
-app.use(shopRoutes);
+// app.use('/admin', adminRoutes);
+// app.use(shopRoutes);
 
 // This is a fallback route when router does not match with any path provided by user
 app.use(errorController.get404);
 
-// Add one to many relation for a user having multiple (shared) products in a cart
-Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
-User.hasMany(Product);
-User.hasOne(Cart);
-Cart.belongsTo(User);
-Cart.belongsToMany(Product, { through: CartItem });
-Product.belongsToMany(Cart, { through: CartItem });
-Order.belongsTo(User);
-User.hasMany(Order);
-Order.belongsToMany(Product, { through: OrderItem });
 
-
-// Establish the database connection and sync the database to app
-sequelize
-  // .sync({ force: true })
-  .sync()
-  .then(result => {
-    return User.findByPk(1);
-    // console.log(result);
-    // app.listen(3000);
-  })
-  .then(user => {
-    // console.log(user);
-    if (!user) {
-      return User.create({ id: 1, name: 'Dhirendra', email: 'test@test.com' });
-    }
-    return user;
-  })
-  .then(user => {
-    // console.log(user);
-    // app.listen(3000);
-    return user.createCart();
-  })
-  .then(cart => {
-    app.listen(3000);
-  })
-  .catch(console.log)
+// Connecting mongodb and listening to server
+mongoConnect((client) => {
+  console.log(client);
+  app.listen(3000);
+})
--- util/database.js
+++ util/database.js
@@ -1,8 +1,19 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
 
-const sequelize = new Sequelize('node-bootcamp', 'root', '1234567', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
+const MongoCleint = mongodb.MongoClient;
 
-module.exports = sequelize;
+// Example URI to connect mongoDB cluster
+// mongodb+srv://node-bootcamp:<password>@cluster0.5j4dk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+
+const mongodbURI = "mongodb+srv://node-bootcamp:node-bootcamp@cluster0.5j4dk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+const mongoConnect = (callback) => {
+  MongoCleint.connect(mongodbURI)
+    .then(client => {
+      console.log('MongoDB Connected');
+      callback(client);
+    })
+    .catch(console.log);
+}
+
+module.exports = mongoConnect;
