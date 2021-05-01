d131a86573915f830f00424188439c64102f1afe
Dhirendra Prajapati <officialprajapati0@gmail.com>
2021-05-01 19:57:34 +0530

    fixed bug for redirecting to cart page after adding product to cart

--- controllers/shop.js
+++ controllers/shop.js
@@ -47,7 +47,7 @@ exports.postCart = (req, res, next) => {
       return req.user.addToCart(product);
     })
     .then(result => {
-      console.log(result);
+      res.redirect('/cart');
     })
     .catch(console.log);
 }
