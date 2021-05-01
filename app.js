8b81701ca92b78740e18270a2def1384341f7a96
Dhirendra Prajapati <officialprajapati0@gmail.com>
2021-05-02 03:05:19 +0530

    adding and using user model

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require('path');
 const errorController = require('./controllers/error');
 
 const mongoose = require('mongoose');
-// const User = require('./models/user');
+const User = require('./models/user');
 
 const app = express();
 
@@ -20,14 +20,14 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(express.static(path.join(__dirname, 'public'))); // This will expose public folder
 
-// app.use((req, res, next) => {
-//   User.findById('608b41d7bfb0343c2afbfe0e') // user id is entered manually
-//     .then(user => {
-//       req.user = new User(user.name, user.email, user.cart, user._id);
-//       next();
-//     })
-//     .catch(console.log);
-// })
+app.use((req, res, next) => {
+  User.findById('608dc9691db2f329807fd513') // user id is entered manually
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(console.log);
+})
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -39,7 +39,17 @@ app.use(errorController.get404);
 // Connecting mongodb and listening to server
 mongoose.connect("mongodb+srv://node-bootcamp:node-bootcamp@cluster0.5j4dk.mongodb.net/shop?retryWrites=true&w=majority")
   .then(reuslt => {
-    console.log('Mongoose Connected');
+    User.findOne()
+      .then(user => {
+        if (!user) {
+          const user = new User({
+            name: 'dhirendra',
+            email: 'dhirendra@test.com',
+            cart: { items: [] }
+          })
+          user.save();
+        }
+      })
     app.listen(3000);
   })
   .catch(console.log)
