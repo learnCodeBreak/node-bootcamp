f3007a75c845e9e524c345a23aa2a63d1b2c30c8
Dhirendra Prajapati <officialprajapati0@gmail.com>
2021-05-03 01:32:42 +0530

    fix minor bugs

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,7 +4,7 @@ exports.getLogin = (req, res, next) => {
   res.render('auth/login', {
     path: '/login',
     pageTitle: 'Login',
-    isAuthenticated: false
+    isAuthenticated: req.session.isLoggedIn
   })
 }
 
