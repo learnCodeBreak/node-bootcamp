54c2e4062ec0d1336754e48cfa00342de2475554
Dhirendra Prajapati <officialprajapati0@gmail.com>
2021-05-02 19:24:49 +0530

    getting and displaying the orders

--- controllers/shop.js
+++ controllers/shop.js
@@ -69,8 +69,9 @@ exports.getCart = (req, res, next) => {
 }
 
 exports.getOrders = (req, res, next) => {
-  req.user
-    .getOrder()
+  Order.find({
+      'user.userId': req.user._id
+    })
     .then(orders => {
       res.render('shop/orders', {
         path: '/orders',
