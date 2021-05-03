bb0222af785bb1b9115d99c2878928532b02226a
Dhirendra Prajapati <officialprajapati0@gmail.com>
2021-05-04 02:18:07 +0530

    advanced authentication with sending mail features

--- app.js
+++ app.js
@@ -11,13 +11,14 @@ const flash = require('connect-flash');
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
-const MONGODB_URI = "mongodb+srv://node-bootcamp:node-bootcamp@cluster0.5j4dk.mongodb.net/shop"
+const MONGODB_URI =
+  'mongodb+srv://maximilian:9u4biljMQc4jjqbe@cluster0-ntrwp.mongodb.net/shop';
 
 const app = express();
 const store = new MongoDBStore({
   uri: MONGODB_URI,
   collection: 'sessions'
-})
+});
 const csrfProtection = csrf();
 
 app.set('view engine', 'ejs');
@@ -27,11 +28,8 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const authRoutes = require('./routes/auth');
 
-
-app.use(bodyParser.urlencoded({
-  extended: false
-}));
-app.use(express.static(path.join(__dirname, 'public'))); // This will expose public folder
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(
   session({
     secret: 'my secret',
@@ -47,31 +45,31 @@ app.use((req, res, next) => {
   if (!req.session.user) {
     return next();
   }
-  User.findById(req.session.user._id) // user id is entered manually
+  User.findById(req.session.user._id)
     .then(user => {
       req.user = user;
       next();
     })
-    .catch(console.log);
-})
+    .catch(err => console.log(err));
+});
 
 app.use((req, res, next) => {
   res.locals.isAuthenticated = req.session.isLoggedIn;
   res.locals.csrfToken = req.csrfToken();
   next();
-})
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
 
-// This is a fallback route when router does not match with any path provided by user
 app.use(errorController.get404);
 
-
-// Connecting mongodb and listening to server
-mongoose.connect(MONGODB_URI)
-  .then(reuslt => {
+mongoose
+  .connect(MONGODB_URI)
+  .then(result => {
     app.listen(3000);
   })
-  .catch(console.log)
+  .catch(err => {
+    console.log(err);
+  });
